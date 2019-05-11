801f57a76d61bf457f4893f7d3e99d2b28d33684
Batsirai Swiswa <batsiraiswiswa@yahoo.com>
2019-05-11 13:11:10 -0400

    Alan Kay - Doing With Images Makes Symbols part 2

---
+++ Programming/lecture6.scm
@@ -0,0 +1,20 @@
+#|
+Alan Kay: Doing With Images Makes Symbols Part 2
+In every application we use these days the applications have windows and icons we click on and take for granted. It is amazing that we now have this standard today. But it was not arrived at by accident.
+
+Book recommendation - Tim Ferris - The Inner Game of Tennis
+
+Learning happens when you get the mentality that needs to learn to focus. You also need to remove the focus from the distracting mentalities. The other problem with teaching is that the part of your body that plays say tennis doesn't understand English so a good way to learn is to find a way of short-circuiting the mentality that always tries to take over. The problem with the usual way of learning something is that you spend a lot of time in doing beginner activities. Instead, you should focus on getting beginners into the activity that they would be doing eventually which is hitting the ball. 
+This principle was applied in user interface design - why spend two weeks reading a user manual to see how to open a file? It is much better to design the interface so that a new user can begin acting as an intermediary from the first time they sit down to use it. And then they get into a process that is rewarding in itself.
+
+Logic depends on the frames of inference. It is subject to the context to which it is applied. The context is much powerful. If you want to be good at solving problems you have to find a context that is going to do most of the thinking for you. An example of this in computer science is to choose the correct data structure before you start tinkering with the algorithm. Choose the right data structure and you will have most of the problem solved before you do any tinkering.
+
+Problem-solving has multiple dimensions. Thus if you are thinking of something in the wrong way, you will most likely not find the solution by doubling your efforts in that frame of mind.
+
+Results-mode interaction -> change the context of what you are working with should change the actions that are available to you eg clicking on a picture should give you actions that are related to images.
+
+According to Alan Kay, computer science and engineering are art forms. Both of them are sterile without an aesthetic feeling. The liberal arts are the best place to develop that aesthetic feeling. People should get a strong liberal arts background and use that as an endless well for metaphors and ideas that can then be used in science and engineering. When reading about famous scientists one will find that the strongest connection they have with their work is not technical but aesthetic so finding the aesthetic in yourself and developing it is the biggest challenge.
+A big challenge for any professional is renewal. Renewal means always being able to sidle up to a new problem and being able to see it from a fresh point of view. As we get older it is harder and harder to see things from a fresh point of view. Renewal can be improved by stepping away from the problem or field and refreshing your mind eg  embarking on aesthetic arts eg music (these engagements will vary for every individual)
+
+The goal of creating a user interface is to make a system that everybody can use without losing any of the capabilities of the machine. How can you make a system that lets you do anything?
+
