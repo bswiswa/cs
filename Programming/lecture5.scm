d56150cc6e4ab3aa3748a7f924eeaf3966d4cfaa
Batsirai Swiswa <batsiraiswiswa@yahoo.com>
2019-05-11 11:46:46 -0400

    Alan Kay - User Interfaces Part 1

---
+++ Programming/lecture5.scm
@@ -0,0 +1,5 @@
+#| Video of Alan Kay on user interface design. Video is called Alan Kay: Doing with Images Makes Symbols Part 1 & 2
+- program called Sketchpad that allowed basic 2-D shape drawing and later 3-D renderings
+- the first instance of a mouse. However there were still too many steps needed in user interaction with the computer. The goal was to make the computer as intimate as possible
+- the move away from the view of computers as a complicated machine used by experts to the view of them as media like paper and pencils and with this view the push to make them usable by children and hoping that it would spill over to adults -> simplicity
+- use of the object-oriented language SmallTalk by children to create impressive animations with only 2 pages of code at most.
