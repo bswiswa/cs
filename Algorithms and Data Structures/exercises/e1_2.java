b3b1e21119b67eb4b7ec210b0aee54088985a044
Batsirai Swiswa <batsiraiswiswa@yahoo.com>
2020-07-18 13:05:36 -0400

    1.2.3

--- Algorithms and Data Structures/exercises/e1_2.java
+++ Algorithms and Data Structures/exercises/e1_2.java
@@ -4,6 +4,12 @@
  *  Last modified:     2020
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.Interval1D;
+import edu.princeton.cs.algs4.Interval2D;
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
 public class e1_2 {
     /// 1.2.1
     /* write a Point2D client that takes an integer value n from the command line, generates
@@ -56,5 +62,46 @@ public class e1_2 {
     }
 
     */
+
+    /* 1.2.3 Write an Interval2D client that takes command-line arguments n, min and max and
+     * generates n random 2D intervals whose width and height are uniformly distributed between min and
+     * max in the unit square. Draw them on StdDraw and print the number of pairs of intervals
+     * that intersect and the number of intervals that are contained in one another */
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        double min = Double.parseDouble(args[1]);
+        double max = Double.parseDouble(args[2]);
+        Interval1D[] x1d = new Interval1D[n];
+        Interval1D[] y1d = new Interval1D[n];
+        Interval2D[] intervals2d = new Interval2D[n];
+        for (int i = 0; i < n; i++) {
+            double xmin = StdRandom.uniform(min, max);
+            x1d[i] = new Interval1D(xmin, StdRandom.uniform(xmin, max));
+            double ymin = StdRandom.uniform(min, max);
+            y1d[i] = new Interval1D(ymin, StdRandom.uniform(ymin, max));
+            intervals2d[i] = new Interval2D(x1d[i], y1d[i]);
+            // draw
+            intervals2d[i].draw();
+        }
+
+        int intersectionCount = 0, containedCount = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j < n; j++) {
+                if (intervals2d[i].intersects(intervals2d[j])) {
+                    intersectionCount++;
+                    if (
+                            (intervals2d[i].contains(new Point2D(x1d[j].min(), y1d[j].min()))
+                                    && intervals2d[i]
+                                    .contains(new Point2D(x1d[j].max(), y1d[j].max())))
+                                    || (
+                                    intervals2d[j].contains(new Point2D(x1d[i].min(), y1d[i].min()))
+                                            && intervals2d[j]
+                                            .contains(new Point2D(x1d[i].max(), y1d[i].max())))
+                    )
+                        containedCount++;
+                }
+            }
+        StdOut.println("Intersections: " + intersectionCount + " Contained: " + containedCount);
+    }
 
 }
